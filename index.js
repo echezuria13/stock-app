c8ab54e0734d6b950d2cf3180a301dabf0e30855
oscar <echezuriaoscar@gmail.com>
2023-02-11 20:27:18 -0400

    nuevo mensaje

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ const port = process.env.PORT || 4000;
 
 app.listen(port);
 
+app.get("/", (req, res) => {
+  res.send("Bienvenido a mi pagina de inicio");
+});
+
 console.log(`listen on port ${port}`);
